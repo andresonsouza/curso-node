6ec04bff80f852db86b933e4d298592a8b177611
Andreson Souza <andresonjazz@gmail.com>
2026-06-08 19:44:36 -0300

    Respondendo requisições com base na url requisitada

--- noticias.js
+++ noticias.js
@@ -1,7 +1,20 @@
-const http = require('http');
+var http = require('http');
 
-const server = http.createServer( function(req, res) {
-    res.end("<html><body><h1>Portal de Noticias</h1><p>Noticia 1</p><p>Noticia 2</p><p>Noticia 3</p></body></html>");
-});
+var server = http.createServer(function (req, res) {
+    
+    var categoria = req.url;
 
-server.listen(3000);
+    if (categoria == '/tecnologia') {
+        res.end("<html><body><h1>Noticias de Tecnologia</h1><p>Noticia 1</p><p>Noticia 2</p><p>Noticia 3</p></body></html>");
+    } 
+    else if (categoria == "/moda") {
+        res.end("<html><body><h1>Noticias de Moda</h1><p>Noticia 1</p><p>Noticia 2</p><p>Noticia 3</p></body></html>");
+    } 
+    else if (categoria == "/esportes") {
+        res.end("<html><body><h1>Noticias de Esportes</h1><p>Noticia 1</p><p>Noticia 2</p><p>Noticia 3</p></body></html>");
+    }
+    else {
+        res.end("<html><body><h1>Portal de Noticias</h1><p>Noticia 1</p><p>Noticia 2</p><p>Noticia 3</p></body></html>");
+    }
+
+}).listen(3000);
